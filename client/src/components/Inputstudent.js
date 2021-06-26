9bfe23c753fdb0bba846f64620fcf4406d2508a6
nameer <79789856+meeru-0000@users.noreply.github.com>
2021-06-26 14:56:47 +0500

    upadate

--- client/src/components/Inputstudent.js
+++ client/src/components/Inputstudent.js
@@ -14,7 +14,7 @@ class Inputstudent extends React.Component
         this.setState({[e.target.name]:e.target.value})
     }
     handleSubmit = ()=>{
-        if(this.state.firstname!='' && this.state.lastname!='',this.state.place!='')
+        if(this.state.firstname!=='' && this.state.lastname!=='',this.state.place!=='')
         {
             axios.post('http://localhost:5000/students',this.state)
             .then(res=>{
